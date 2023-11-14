3843d9761e60baef13e483e64040b769a2453115
Znurana <nzeynalli@std.beu.edu.az>
2023-11-14 15:06:08 +0400

    changes

---
+++ src/app/shared/payment-detail.model.ts
@@ -0,0 +1,9 @@
+export class PaymentDetail {
+    
+        paymentDetailId:number= 0
+        cardOwnerName: string=""
+        cardNumber: string=""
+        securityCode: string=""
+        expirationDate: string=""
+      
+}
